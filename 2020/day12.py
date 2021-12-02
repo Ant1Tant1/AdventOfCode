211b69aee7499fd956ad2e4755c14e4c12201943
AteTt1 <antoine@neuroflex.io>
2021-12-02 11:46:14 -0800

    [2020][day 12][Refactoring]

--- 2020/day12.py
+++ 2020/day12.py
@@ -1,6 +1,6 @@
 from utils import readfile
 import numpy as np
-from math import sin, cos
+from math import sin, cos, pi
 
 data = readfile("2020/day12.txt")
 
@@ -28,8 +28,8 @@ def get_action(i=0, facing_dir=0, dir_x=0, dir_y=0):
     elif char == 'L':
         facing_dir += value
     elif char == 'F':
-        dir_y += round(sin(facing_dir*math.pi/180) * value, 1)
-        dir_x += round(cos(facing_dir*math.pi/180) * value, 1)
+        dir_y += round(sin(facing_dir*pi/180) * value, 1)
+        dir_x += round(cos(facing_dir*pi/180) * value, 1)
     
     return get_action(i+1, facing_dir, dir_x, dir_y)
 
@@ -50,12 +50,12 @@ def get_action_2(i=0, ship_pos=np.array([0,0]), dir=np.array([10,1])):
     elif char == 'W':
         dir[0] -= value
     elif char == 'R':
-        new_x = (dir[0] * cos(-value*math.pi/180) - dir[1] * sin(-value*math.pi/180))
-        new_y = (dir[0] * sin(-value*math.pi/180) + dir[1] * cos(-value*math.pi/180))
+        new_x = (dir[0] * cos(-value*pi/180) - dir[1] * sin(-value*pi/180))
+        new_y = (dir[0] * sin(-value*pi/180) + dir[1] * cos(-value*pi/180))
         dir = [round(new_x, 1), round(new_y, 1)]
     elif char == 'L':
-        new_x = (dir[0] * cos(value*math.pi/180) - dir[1] * sin(value*math.pi/180))
-        new_y = (dir[0] * sin(value*math.pi/180) + dir[1] * cos(value*math.pi/180))
+        new_x = (dir[0] * cos(value*pi/180) - dir[1] * sin(value*pi/180))
+        new_y = (dir[0] * sin(value*pi/180) + dir[1] * cos(value*pi/180))
         dir = [round(new_x, 1), round(new_y, 1)]
     elif char == 'F':
         ship_pos[0] = ship_pos[0] + value * dir[0]
