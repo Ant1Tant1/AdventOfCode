1be66d3f5b9aab32a864d4dc0104c7725fcd7073
AteTt1 <antoine@neuroflex.io>
2021-12-07 09:03:35 -0800

    [2021][day-7] added best solution for pb 1

--- 2021/day-7.py
+++ 2021/day-7.py
@@ -4,14 +4,20 @@ import numpy as np
 # read data
 data = readfile(r"2021/day-7.txt", my_type=str)
 
-# data = ["16,1,2,0,4,2,7,1,2,14"]
+data = ["16,1,2,0,4,2,7,1,2,14"]
 data = np.array(data[0].split(','), dtype=int)
 
-# pb 1
+# ===========  PB 1  ===========
+######## BEST SOLUTION #########
+best_pos = np.median(data)
+res = np.sum(np.abs(best_pos - data))
+print(res)
+################################
+
 n = max(data)
 val = [np.sum(np.abs(data - i)) for i in range(n)]
 print(min(val))
 
-# pb 2
+# ===========  PB 2  ===========
 val = [np.sum(np.abs(data - i) * (np.abs(data - i) + 1) / 2) for i in range(n)]
 print(min(val))
