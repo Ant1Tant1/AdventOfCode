1669d14417ef110a6b579a9b0b2f7ed2a6ab6311
AteTt1 <antoine@neuroflex.io>
2022-12-11 12:54:06 -0800

    day 1 & 2 2022

---
+++ 2022/day1.py
@@ -0,0 +1,19 @@
+from utils import readfile
+import numpy as np
+
+# read data
+data = readfile(r"day1.txt")
+
+res = []
+r= 0
+for d in data:
+    if d == '':
+        res.append(r)
+        r = 0
+    else:
+        r += float(d)
+res.append(r)
+
+print("first answer: ", np.max(res))
+
+final_answer = np.sum(np.sort(res)[-3:])
---
+++ 2022/day2.py
@@ -0,0 +1,61 @@
+from utils import readfile
+import numpy as np
+
+# read data
+data = readfile(r"day2.txt")
+
+res = 0
+for d in data:
+    a, b = d.split(" ")
+    
+    if (a == "A") & (b == "Y") \
+    | (a == "B") & (b == "Z") \
+    | (a == "C") & (b == "X"):
+        res += 6
+    elif (a == "A") & (b == "X") \
+    | (a == "B") & (b == "Y") \
+    | (a == "C") & (b == "Z"):
+        res += 3
+    else:
+        res += 0
+
+    if b == "X":
+        res += 1
+    elif b == "Y":
+        res += 2
+    else:
+        res += 3
+
+print("first problem: ", res)
+
+res = 0
+for d in data:
+    a, b = d.split(" ")
+
+    if b == "X":
+        if a == "A":
+            res += 3
+        elif a == "B":
+            res += 1
+        elif a == "C":
+            res += 2
+    
+    if b == "Y":
+        res += 3
+        if a == "A":
+            res += 1
+        elif a == "B":
+            res += 2
+        elif a == "C":
+            res += 3
+
+    if b == "Z":
+        res += 6
+        if a == "A":
+            res += 2
+        elif a == "B":
+            res += 3
+        elif a == "C":
+            res += 1
+
+print("second problem: ", res)
